918e240bd7d741591e24d8e860f8c3334e5a2521
Nitin <nitin3emails@gmail.com>
2020-12-08 13:44:49 +0530

    Added csrfToken

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,18 +2,30 @@ const bycrypt = require("bcrypt");
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: false,
+    errorMessage: message,
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
-    isAuthenticated: false,
+    errorMessage: message,
   });
 };
 
@@ -23,6 +35,7 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
+        req.flash("error", "Invalid Email or Password");
         return res.redirect("/login");
       }
       bycrypt
@@ -36,6 +49,8 @@ exports.postLogin = (req, res, next) => {
               return res.redirect("/");
             });
           }
+
+          req.flash("error", "Invalid Email or Password");
           return res.redirect("/login");
         })
         .catch((err) => console.log(err));
@@ -51,6 +66,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
+        req.flash("error", "User Exist");
         return res.redirect("/signup");
       }
       return bycrypt
