8e9ffeca08fea9f648214f20cbb00c5fb822d3df
Nitin <nitin3emails@gmail.com>
2020-12-10 18:01:23 +0530

    Added asynchronous request in admin product deletion

--- controllers/admin.js
+++ controllers/admin.js
@@ -201,8 +201,28 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+// exports.postDeleteProduct = (req, res, next) => {
+//   const prodId = req.body.productId;
+//   Product.findById(prodId)
+//     .then((prod) => {
+//       if (!prod) {
+//         return next(new Error("Product not found"));
+//       }
+//       fileHelper.deleteFile(prod.imageUrl);
+//       return Product.findOneAndDelete({ _id: prodId, userId: req.user._id });
+//     })
+//     .then(() => {
+//       console.log("DESTROYED PRODUCT");
+//       res.redirect("/admin/products");
+//     })
+//     .catch((err) => {
+//       const error = new Error(err);
+//       error.httpStatusCode = 500;
+//       return next(error);
+//     });
+// };
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then((prod) => {
       if (!prod) {
@@ -213,11 +233,9 @@ exports.postDeleteProduct = (req, res, next) => {
     })
     .then(() => {
       console.log("DESTROYED PRODUCT");
-      res.redirect("/admin/products");
+      res.status(200).json({ message: "Success" });
     })
     .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({ message: "Deleting product failed !" });
     });
 };
